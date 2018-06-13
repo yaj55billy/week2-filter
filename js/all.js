2bb8677e64dc23b5335ce7c00463767775dd1573
紀冠逸 <yaj55ushydna@gmail.com>
2018-06-13 09:24:22 +0800

    js search select

--- js/all.js
+++ js/all.js
@@ -37,7 +37,7 @@ xhr.onload = function(){
     //location 篩選
     function areaFilter(){
         var locationValue = document.getElementById('location').value;
-        console.log(locationValue); //選到就印出那個區域
+        // console.log(locationValue); //選到就印出那個區域
 
         var filterData =[];
 
@@ -47,17 +47,34 @@ xhr.onload = function(){
             //存取 dataArea 為這個物件的區域
             var dataArea = data.result.records[i].Zone;
 
-            //經過篩選資料的 name
-            var dataName = data.result.records[i].Name;
-        
             if(locationValue == dataArea){
                 //filterData 為經過篩選的資料 +入陣列
-                filterData.push(data.result.records[i]);
-
-                updateContent(filterData,dataName);
+                filterData.push(data.result.records[i]);   
             }
         }
-        
+        updateContent(filterData);
+    }
+
+    //將地區選擇存取，如果發生change事件則執行areaFilter
+    var searchBtn = document.querySelector('.search button');
+    searchBtn.addEventListener('click',searchFilter);
+
+    function searchFilter(){
+        var searchTextValue = document.getElementById('search-text').value;
+        // console.log(searchTextValue);
+
+        var filterData =[];
+        for (var i = 0; i< dataLen; i++) {
+
+            //存取 相關資訊 為這個物件的區域
+            var dataDescription = data.result.records[i].Description;
+            // 判斷變數dataDescription值是否包含 searchTextValue,如果dataDescription.indexOf(searchTextValue)不等於-1說明包含 “searchTextValue” 為true繼續執行
+            if(dataDescription.indexOf(searchTextValue)!=-1){
+                //filterData 為經過篩選的資料 +入陣列
+                filterData.push(data.result.records[i]);   
+            }
+        }
+        updateContent(filterData);
     }
 }
 
@@ -66,21 +83,20 @@ var content = document.querySelector('.content-list');
 
 // 組字串
 
-function updateContent(filterData,dataName){
+function updateContent(filterData){
     var str = '';
-    // console.log(dataName);
-    console.log(filterData.length);
-    // console.log(filterData[i].dataName);
-    
+    var dataTotal = document.querySelector('.dataTotal');
+    dataTotal.textContent = filterData.length;
+    // console.log(filterData);
+    // console.log(filterData.length);
+    // Description
     for (var i = 0; i< filterData.length; i++) {
-        // console.log(filterData[i]);
-        console.log(dataName);
         str = str +  `
         <li class="row">
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 list-img" style="background-image: url(images/content.png)"></div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 list-img" style="background-image: url(${filterData[i].Picture1})"></div>
             <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 list-text">
-                <h2>${21}</h2>
-                <p>Donec euismod scelerisque ligula. Maecenas eu varius risus, eu aliquet arcu. Curabitur fermentum suscipit est, tincidunt mattis lorem luctus id. Donec eget massa a diam condimentum pretium. Aliquam erat volutpat. Integer ut tincidunt orci. Etiam tristique, elit ut consectetur iaculis, metus lectus mattis justo, vel mollis eros neque quis augue. Sed lobortis ultrices lacus, a placerat metus rutrum sit amet. Aenean ut suscipit justo.</p>
+                <h2>${filterData[i].Name}</h2>
+                <p>${filterData[i].Description}</p>
                 <div style="margin-bottom: 16px;">
                     <span class="list-organizer">
                         Ethan Foster
@@ -94,9 +110,9 @@ function updateContent(filterData,dataName){
                         <i class="fa fa-map-marker" aria-hidden="true"></i>
                         Kaohsiung City
                     </span>
-                    <span class="list-time">
-                        <i class="fa fa-calendar" aria-hidden="true"></i>
-                        2018/5/24 - 2018/5/31
+                    <span class="list-phone">
+                        <i class="fa fa-phone" aria-hidden="true"></i>
+                        ${filterData[i].Tel}
                     </span>
                 </div>
             </div>
